56ebd3ce2af19cc7ae9a379650e6f5b0edf64677
Cloudy1112 <chonglevan2004@gmai.com>
2025-11-15 15:32:57 +0700

    Main Activity Layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("de.hdodenhof:circleimageview:3.1.0") //thư viện để tạo ảnh góc bo tròn
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
